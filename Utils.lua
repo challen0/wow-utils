41d7dd075b5f49baaab5db7e816fbd36c8a6bb38
Chris Allen <chrisallen@mailbolt.com>
2020-08-04 22:52:51 -0600

    auto repair on merchant frame show

--- Utils.lua
+++ Utils.lua
@@ -1,7 +1,25 @@
-local function MerchantShow_GuildRepair(self, event)
-	
+GUILD_REPAIR_MESSAGE = "Repairing your items for %s from guild funds. You have %s left for today."
+OWN_REPAIR_MESSAGE = "Repairing your items for %s (%s from guild funds and %s from your funds)."
+
+function MerchantShow_AutoRepair(self, event)
+	if (CanMerchantRepair()) then
+		local repairAllCost, needRepairs = GetRepairAllCost()
+		if (needRepairs and CanGuildBankRepair()) then
+			local guildBankWithdrawLimit = GetGuildBankWithdrawMoney()
+			local repairAllCoinText = GetCoinText(repairAllCost, ", ")
+			if (repairAllCost < guildBankWithdrawLimit) then
+				local guildBankWithdrawLimitCoinText = GetCoinText(guildBankWithdrawLimit - repairAllCost, ", ")
+				DEFAULT_CHAT_FRAME:AddMessage(string.format(GUILD_REPAIR_MESSAGE, repairAllCoinText, guildBankWithdrawLimitCoinText), 255, 255, 0)
+			else
+				local guildFundsCoinText = GetCoinText(guildBankWithdrawLimit, ", ")
+				local ownFundsCoinText = GetCoinText(abs(repairAllCost - guildBankWithdrawLimit), ", ")
+				DEFAULT_CHAT_FRAME:AddMessage(string.format(OWN_REPAIR_MESSAGE, repairAllCoinText, guildFundsCoinText, ownFundsCoinText), 255, 255, 0)
+			end
+			-- RepairAllItems(true)
+		end
+	end
 end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("MERCHANT_SHOW")
-f:SetScript("OnEvent", MerchantShow_GuildRepair)
+f:SetScript("OnEvent", MerchantShow_AutoRepair)
