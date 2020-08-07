672709ecb9f9d5652469df91790aa91e890d609a
Chris Allen <chrisallen@mailbolt.com>
2020-08-06 20:53:52 -0600

    make all other functions local

--- Utils/AutoRepair.lua
+++ Utils/AutoRepair.lua
@@ -17,7 +17,7 @@ function AutoRepair:OnMerchantShow(event)
     end
 end
 
-function DetermineRepairMessage(repairAllCost)
+local function DetermineRepairMessage(repairAllCost)
     local guildBankWithdrawLimit = GetGuildBankWithdrawMoney()
     if (repairAllCost < guildBankWithdrawLimit) then
         return FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit - repairAllCost)
@@ -26,21 +26,21 @@ function DetermineRepairMessage(repairAllCost)
     end
 end
 
-function FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit)
+local function FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit)
     local GuildRepairMessage = "Repairing your items for %s. You have %s left in the guild bank for today."
     return string.format(GuildRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankWithdrawLimit))
 end
 
-function FormatOwnRepairMessage(repairAllCost, guildBankUsed, ownMoneyUsed)
+local function FormatOwnRepairMessage(repairAllCost, guildBankUsed, ownMoneyUsed)
     local OwnRepairMessage = "Repairing your items for %s with %s from the guild bank and %s of your own money."
     return string.format(OwnRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankUsed), FormatMoney(ownMoneyUsed))
 end
 
-function FormatMoney(money)
+local function FormatMoney(money)
     return GetCoinText(money, ", ")
 end
 
-function PrintRepairMessage(message)
+local function PrintRepairMessage(message)
     DEFAULT_CHAT_FRAME:AddMessage(message, 255, 255, 0)
 end
 
