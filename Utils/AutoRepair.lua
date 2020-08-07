15ea694e856995245c5d09f009778a1616fdff65
Chris Allen <chrisallen@mailbolt.com>
2020-08-06 20:56:17 -0600

    rearrange functions for scoping

--- Utils/AutoRepair.lua
+++ Utils/AutoRepair.lua
@@ -2,6 +2,33 @@ addonName, addonTable = ...
 
 local AutoRepair = {}
 
+local function FormatMoney(money)
+    return GetCoinText(money, ", ")
+end
+
+local function PrintRepairMessage(message)
+    DEFAULT_CHAT_FRAME:AddMessage(message, 255, 255, 0)
+end
+
+local function FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit)
+    local GuildRepairMessage = "Repairing your items for %s. You have %s left in the guild bank for today."
+    return string.format(GuildRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankWithdrawLimit))
+end
+
+local function FormatOwnRepairMessage(repairAllCost, guildBankUsed, ownMoneyUsed)
+    local OwnRepairMessage = "Repairing your items for %s with %s from the guild bank and %s of your own money."
+    return string.format(OwnRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankUsed), FormatMoney(ownMoneyUsed))
+end
+
+local function DetermineRepairMessage(repairAllCost)
+    local guildBankWithdrawLimit = GetGuildBankWithdrawMoney()
+    if (repairAllCost < guildBankWithdrawLimit) then
+        return FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit - repairAllCost)
+    else
+        return FormatOwnRepairMessage(repairAllCost, guildBankWithdrawLimit, repairAllCost - guildBankWithdrawLimit)
+    end
+end
+
 function AutoRepair:OnMerchantShow(event)
     if event == "MERCHANT_SHOW" then
         local canMerchantRepair = CanMerchantRepair()
@@ -15,33 +42,6 @@ function AutoRepair:OnMerchantShow(event)
             end
         end
     end
-end
-
-local function DetermineRepairMessage(repairAllCost)
-    local guildBankWithdrawLimit = GetGuildBankWithdrawMoney()
-    if (repairAllCost < guildBankWithdrawLimit) then
-        return FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit - repairAllCost)
-    else
-        return FormatOwnRepairMessage(repairAllCost, guildBankWithdrawLimit, repairAllCost - guildBankWithdrawLimit)
-    end
-end
-
-local function FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit)
-    local GuildRepairMessage = "Repairing your items for %s. You have %s left in the guild bank for today."
-    return string.format(GuildRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankWithdrawLimit))
-end
-
-local function FormatOwnRepairMessage(repairAllCost, guildBankUsed, ownMoneyUsed)
-    local OwnRepairMessage = "Repairing your items for %s with %s from the guild bank and %s of your own money."
-    return string.format(OwnRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankUsed), FormatMoney(ownMoneyUsed))
-end
-
-local function FormatMoney(money)
-    return GetCoinText(money, ", ")
-end
-
-local function PrintRepairMessage(message)
-    DEFAULT_CHAT_FRAME:AddMessage(message, 255, 255, 0)
 end
 
 addonTable.AutoRepair = AutoRepair
