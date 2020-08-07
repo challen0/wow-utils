d46b54566829f9c2ac09ed6cadaae4e530fe3585
Chris Allen <chrisallen@mailbolt.com>
2020-08-06 22:28:55 -0600

    add main functionality to sell items

--- Utils/SellPoorQuality.lua
+++ Utils/SellPoorQuality.lua
@@ -15,7 +15,23 @@ local function SellItem(bag, slot)
 end
 
 function SellPoorQuality:OnMerchantShow(event)
-
+    if (event == "MERCHANT_SHOW") then
+        local totalSellPrice = 0
+        for bag = 0, 4 do
+            for slot = 1, GetContainerNumSlots(bag) do
+                local _, itemCount, _, quality, _, _, _, _, noValue, itemID = GetContainerItemInfo(bag, slot)
+                if (itemID and quality == 0 and not noValue) then
+                    local stackPrice = itemCount * select(11, GetItemInfo(itemID))
+                    totalSellPrice = totalSellPrice + stackPrice
+                    SellItem(bag, slot)
+                end
+            end
+        end
+        if (totalSellPrice > 0) then
+            local message = FormatSoldMessage(totalSellPrice)
+            PrintMessage(message)
+        end
+    end
 end
 
 addonTable.SellPoorQuality = SellPoorQuality
