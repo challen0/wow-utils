0aa8d532277e9fc9b5186aa771d392aa520426b8
Chris Allen <chrisallen@mailbolt.com>
2020-08-06 22:03:12 -0600

    add function to format sold message

--- Utils/SellPoorQuality.lua
+++ Utils/SellPoorQuality.lua
@@ -4,6 +4,11 @@ local FormatMoney = addonTable.FormatMoney
 local PrintMessage = addonTable.PrintMessage
 local SellPoorQuality = {}
 
+local function FormatSoldMessage(total)
+    local SoldMessage = "Sold all poor quality items for %s"
+    return string.format(SoldMessage, FormatMoney(total))
+end
+
 function SellPoorQuality:OnMerchantShow(event)
 
 end
