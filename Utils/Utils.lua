788be8d5417c1c84c438a89a02ea0d69c644be54
Chris Allen <chrisallen@mailbolt.com>
2020-08-06 21:03:44 -0600

    add new function to event

--- Utils/Utils.lua
+++ Utils/Utils.lua
@@ -3,3 +3,4 @@ addonName, addonTable = ...
 local f = CreateFrame("Frame")
 f:RegisterEvent("MERCHANT_SHOW")
 f:SetScript("OnEvent", addonTable.AutoRepair.OnMerchantShow)
+f:SetScript("OnEvent", addonTable.SellPoorQuality.OnMerchantShow)
