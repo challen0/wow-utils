bf7880d42451dcb2749233282a17e78571316e09
Chris Allen <chrisallen@mailbolt.com>
2020-08-11 22:53:14 -0600

    introduce function for checking if item is bop

--- ChallenUtilities/AutoSellUnusable.lua
+++ ChallenUtilities/AutoSellUnusable.lua
@@ -14,6 +14,10 @@ local function SellItem(bag, slot)
     PickupMerchantItem()
 end
 
+local function IsItemBindOnPickup(bindType)
+    return bindType == 1 -- 1 is bind on pickup
+end
+
 local function GetUsableArmor()
     local _, class, _ = UnitClass('player')
     local usableArmor
