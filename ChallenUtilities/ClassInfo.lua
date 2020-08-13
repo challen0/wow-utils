f9cf1c4b06ec00108bbac0c54f1794a4f689d816
Chris Allen <chrisallen@mailbolt.com>
2020-08-12 22:05:26 -0600

    copy functions into class info file

--- ChallenUtilities/ClassInfo.lua
+++ ChallenUtilities/ClassInfo.lua
@@ -2,6 +2,29 @@ addonName, addonTable = ...
 
 local ClassInfo = {}
 
+function ClassInfo:GetPlayerClass()
+    local class, _ = UnitClassBase('player')
+    return class
+end
 
+function ClassInfo:CanEquipShield(class)
+    return class == 'WARRIOR' or class == 'PALADIN' or class == 'SHAMAN'
+end
+
+function ClassInfo:CanWearPlate(class)
+    return class == 'DEATHKNIGHT' or class == 'PALADIN' or class == 'WARRIOR'
+end
+
+function ClassInfo:CanWearMail(class)
+    return self.CanWearPlate(class) or class == 'HUNTER' or class == 'SHAMAN'
+end
+
+function ClassInfo:CanWearLeather(class)
+    return self.CanWearMail(class) or class == 'DEMONHUNTER' or class == 'DRUID' or class == 'MONK' or class == 'ROGUE'
+end
+
+function ClassInfo:CanWearCloth(class)
+    return self.CanWearLeather(class) or class == 'MAGE' or class == 'PRIEST' or class == 'WARLOCK'
+end
 
 addonTable.ClassInfo = ClassInfo
