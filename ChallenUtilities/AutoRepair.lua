f20456f592e958804542e9258195f08bdbdb09c1
Chris Allen <chrisallen@mailbolt.com>
2020-08-12 22:22:40 -0600

    removed unused functions

--- ChallenUtilities/AutoRepair.lua
+++ ChallenUtilities/AutoRepair.lua
@@ -6,25 +6,6 @@ local function FormatMoney(money)
     return GetCoinText(money, ", ")
 end
 
-local function FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit)
-    local GuildRepairMessage = "Repairing your items for %s. You have %s left in the guild bank for today."
-    return string.format(GuildRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankWithdrawLimit))
-end
-
-local function FormatOwnRepairMessage(repairAllCost, guildBankUsed, ownMoneyUsed)
-    local OwnRepairMessage = "Repairing your items for %s with %s from the guild bank and %s of your own money."
-    return string.format(OwnRepairMessage, FormatMoney(repairAllCost), FormatMoney(guildBankUsed), FormatMoney(ownMoneyUsed))
-end
-
-local function DetermineRepairMessage(repairAllCost)
-    local guildBankWithdrawLimit = GetGuildBankWithdrawMoney()
-    if (repairAllCost < guildBankWithdrawLimit) then
-        return FormatGuildRepairMessage(repairAllCost, guildBankWithdrawLimit - repairAllCost)
-    else
-        return FormatOwnRepairMessage(repairAllCost, guildBankWithdrawLimit, repairAllCost - guildBankWithdrawLimit)
-    end
-end
-
 local function HowMuchOwnFundsUsed(repairAllCost)
     local guildBankWithdrawLimit = GetGuildBankWithdrawMoney()
     return repairAllCost - guildBankWithdrawLimit
