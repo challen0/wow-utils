e23c91345121f138a40ea3e4fdf46de0be15f12b
Chris Allen <30563180+challen0@users.noreply.github.com>
2020-12-13 13:47:36 -0700

    remove-sell-unusable (#25)
    * remove selling unusable
    
    * refactor selling junk
    
    * update toc file
    
    * update readme

--- ChallenUtilities/AutoSellPoorQuality.lua
+++ ChallenUtilities/AutoSellPoorQuality.lua
@@ -1,6 +1,5 @@
 addonName, addonTable = ...
 
-local ItemInfo = addonTable.ItemInfo
 local AutoSellPoorQuality = {}
 
 local function SellItem(bag, slot)
@@ -8,12 +7,16 @@ local function SellItem(bag, slot)
     PickupMerchantItem()
 end
 
+local function IsItemPoorQuality(quality)
+    return quality == 0
+end
+
 function AutoSellPoorQuality:OnMerchantShow(event)
     if (event == "MERCHANT_SHOW") then
         for bag = 0, 4 do
             for slot = 1, GetContainerNumSlots(bag) do
                 local _, _, _, quality, _, _, _, _, noValue, itemID = GetContainerItemInfo(bag, slot)
-                if (itemID and ItemInfo:IsItemPoorQuality(quality) and not noValue) then
+                if (itemID and IsItemPoorQuality(quality) and not noValue) then
                     SellItem(bag, slot)
                 end
             end
--- README.md
+++ README.md
@@ -5,4 +5,3 @@ This addon contains a few helpful utilities that I wanted to add to WoW.
 Right now it includes the following:
 * Automatically repair using the guild bank
 * Automatically sell poor quality (gray) items
-* Automatically sell soulbound unusable armor
